9d3c2c3bad58028f8af45aef747d8dbf0315e5d4
sashaporitskiy <31014377+sashaporitskiy@users.noreply.github.com>
2017-09-04 00:02:23 +0300

    выполнил задание 2

--- src/Dz2/DisplayTxtDz2TXT.java
+++ src/Dz2/DisplayTxtDz2TXT.java
@@ -1,4 +1,18 @@
 package Dz2;
-
+import java.util.Scanner;
 public class DisplayTxtDz2TXT {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите имя:");
+        String name = in.nextLine();
+        System.out.println("Введите город:");
+        String gorod = in.nextLine();
+        System.out.println("Введите возраст:");
+        String vozrast = in.nextLine();
+        System.out.println("Введите хобби:");
+        String hoby = in.nextLine();
+        System.out.println("Человек по имени "+name+" живет в городе " +gorod+ ".");
+        System.out.println("Этому человеку "+vozrast+" лет и любит он заниматься "+hoby+".");
+
+    }
 }
