fe6b518d7e5d9e579448949b7416d9f5cf5e14b2
sashaporitskiy <31014377+sashaporitskiy@users.noreply.github.com>
2017-09-03 18:52:26 +0300

    Добавил еще одну строку в вывод текста

--- src/Dz1/DisplayTXT.java
+++ src/Dz1/DisplayTXT.java
@@ -3,6 +3,6 @@ package Dz1;
 public class DisplayTXT {
     public static void main(String[] args) {
         System.out.println("Как я проведу лето 2017");
-        
+        System.out.println("Провел очень хорошо");
     }
 }
