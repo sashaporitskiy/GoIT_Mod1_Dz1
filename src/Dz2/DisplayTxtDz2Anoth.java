2a87b7b619bbe5fd4ed6defe413d834354af4f2f
sashaporitskiy <31014377+sashaporitskiy@users.noreply.github.com>
2017-09-04 00:08:13 +0300

    выполнил подзадание 3

---
+++ src/Dz2/DisplayTxtDz2Anoth.java
@@ -0,0 +1,19 @@
+package Dz2;
+import java.util.Scanner;
+public class DisplayTxtDz2Anoth {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите имя:");
+        String name = in.nextLine();
+        System.out.println("Введите город:");
+        String gorod = in.nextLine();
+        System.out.println("Введите возраст:");
+        String vozrast = in.nextLine();
+        System.out.println("Введите хобби:");
+        String hoby = in.nextLine();
+        System.out.println(name+" - Имя");
+        System.out.println(gorod+" - Город");
+        System.out.println(vozrast+" - Возраст");
+        System.out.println(hoby+" - Хобби");
+    }
+}
