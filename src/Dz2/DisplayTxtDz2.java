0b330157986a76e6482081b3e811837980d5194d
sashaporitskiy <31014377+sashaporitskiy@users.noreply.github.com>
2017-09-11 19:03:26 +0300

    проверка работы

--- src/Dz2/DisplayTxtDz2.java
+++ src/Dz2/DisplayTxtDz2.java
@@ -6,6 +6,7 @@ public class DisplayTxtDz2 {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
+
         System.out.println("Введите имя:");
         String name = in.nextLine();
         System.out.println("Введите город:");
--- src/Dz2/DisplayTxtDz2Anoth.java
+++ src/Dz2/DisplayTxtDz2Anoth.java
@@ -2,6 +2,7 @@ package Dz2;
 import java.util.Scanner;
 public class DisplayTxtDz2Anoth {
     public static void main(String[] args) {
+
         Scanner in = new Scanner(System.in);
         System.out.println("Введите имя:");
         String name = in.nextLine();
